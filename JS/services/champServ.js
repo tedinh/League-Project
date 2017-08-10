8d2a43a8592b5813918f95135457b691d7af33f7
Terry Dinh <terrydinh@Terrys-MacBook-Pro.local>
2017-08-10 09:22:42 -0500

    api update

--- JS/services/champServ.js
+++ JS/services/champServ.js
@@ -2,7 +2,7 @@ angular.module("league").service("champServ", function($http) {
   this.getChampData = function() {
     return $http
       .get(
-        "https://na1.api.riotgames.com/lol/static-data/v3/champions?api_key=RGAPI-02408561-a872-4317-a707-d4d62ea341cc"
+        "https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key=RGAPI-02408561-a872-4317-a707-d4d62ea341cc"
       )
       .then(function(result) {
         var arr = [];
